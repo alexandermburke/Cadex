4a50ef1a8d209aa418060e63f77578c984683598
Alex Burke <10841351+SupremacyXI@users.noreply.github.com>
2024-08-15 12:26:05 -0700

    Update head.js

--- Development/web/src/app/head.js
+++ Development/web/src/app/head.js
@@ -1,5 +1,5 @@
 import Script from 'next/script';
-import React from 'react'
+import React from 'react';
 
 export default function Head() {
     return (
@@ -7,6 +7,8 @@ export default function Head() {
             <link rel="icon" href="/favicon.png" sizes="any" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
             <title>Cadex Law Simulator</title>
+            <meta name="description" content="Cadex Law Simulator: Enhance your legal practice with AI-powered simulations. Practice real-life legal scenarios, court cases, and more." />
+            <meta name="keywords" content="Cadex, law simulation, court simulations, legal practice, LSAT prep, legal education, 7Sage alternative, courtroom experience, legal training, bar exam prep, mock trials, legal scenarios, legal technology, AI legal simulations" />
         </head>
     )
 }
