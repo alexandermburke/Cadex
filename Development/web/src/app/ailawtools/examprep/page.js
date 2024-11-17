af4996a8ef49ac8d133503c2d48175f938e7e37d
Alex Burke <10841351+SupremacyXI@users.noreply.github.com>
2024-11-16 22:08:46 -0700

    6

--- Development/web/src/app/ailawtools/examprep/page.js
+++ Development/web/src/app/ailawtools/examprep/page.js
@@ -1,9 +1,7 @@
 import ExamPrep from '@/components/tools/ExamPrep'
-import CoolLayout from '@/components/CoolLayout'
-import Main from '@/components/Main'
 import React from 'react'
 
-export default function ApplicationPage() {
+export default function BillingPage() {
     return (
         <ExamPrep />
     )
