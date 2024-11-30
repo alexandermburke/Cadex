c8e3819ca34aeedfcf2e8b4492230dd8921f4620
Alex Burke <10841351+SupremacyXI@users.noreply.github.com>
2024-11-29 21:23:10 -0700

    Footer/LogoFiller

--- Development/web/src/components/LogoFiller.js
+++ Development/web/src/components/LogoFiller.js
@@ -1,4 +1,5 @@
 import { Poppins } from 'next/font/google';
+import Image from 'next/image';
 import React from 'react';
 
 const poppins = Poppins({ subsets: ["latin"], weight: ['400', '100', '200', '300', '500', '600', '700'] });
@@ -6,8 +7,21 @@ const poppins = Poppins({ subsets: ["latin"], weight: ['400', '100', '200', '300
 export default function LogoFiller() {
     return (
         <div className='grid place-items-center flex-1 text-center text-slate-300'>
-            <div className='flex flex-col gap-4 font-medium'>
-                <h4 className={'text-xl px-3 sm:text-2xl goldGradient font-medium sm:px-4 ' + poppins.className}>Cadex Law</h4>
+            <div className='flex flex-col items-center gap-2 font-medium'>
+                {/* Header Image */}
+                <div className="relative w-10 h-10 sm:w-12 sm:h-12 mx-auto my-auto">
+                    <Image
+                        src="/header.png" 
+                        alt="Cadex Law Logo"
+                        layout="fill"
+                        objectFit="contain"
+                        className="rounded-none" 
+                    />
+                </div>
+                {/* Text Content */}
+                <h4 className={'text-xl px-3 sm:text-2xl goldGradient font-medium sm:px-4 ' + poppins.className}>
+                    Cadex Law
+                </h4>
                 <p className='text-blue-950'>
                     Elevate your legal studies<br />
                     Dive into interactive case simulations to enhance your skills
