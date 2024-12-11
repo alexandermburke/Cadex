77e8c36526f02785ce92c61fd7d898abee86d854
Alex Burke <10841351+SupremacyXI@users.noreply.github.com>
2024-12-10 20:02:57 -0700

    Update Footer.js

--- Development/web/src/components/Footer.js
+++ Development/web/src/components/Footer.js
@@ -12,7 +12,7 @@ const poppins = Poppins({
 
 export default function Footer() {
   // You can set your version number here
-  const versionNumber = '0.8.3';
+  const versionNumber = '0.8.7';
 
   return (
     <footer className="flex flex-col bg-gradient-to-r from-blue-950 to-slate-800 text-white py-4">
