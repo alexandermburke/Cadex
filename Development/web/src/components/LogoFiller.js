22187ca60ac1ba279438f41a83f799db0ef19f92
Alex Burke <10841351+SupremacyXI@users.noreply.github.com>
2025-01-05 22:18:14 -0700

    Update LogoFiller.js

--- Development/web/src/components/LogoFiller.js
+++ Development/web/src/components/LogoFiller.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import { Poppins } from 'next/font/google';
 import Image from 'next/image';
-// Import your AuthContext or theme context
 import { useAuth } from '@/context/AuthContext';
 
 const poppins = Poppins({
@@ -12,45 +11,38 @@ const poppins = Poppins({
 });
 
 export default function LogoFiller() {
-  // Grab your dark mode setting from Auth or theme context
   const { userDataObj } = useAuth() || {};
   const isDarkMode = userDataObj?.darkMode || false;
 
   return (
     <div
-      className={`
-        grid place-items-center flex-1 text-center 
-        ${isDarkMode ? 'text-gray-300' : 'text-slate-300'}
-      `}
+      className={`grid place-items-center flex-1 text-center ${
+        isDarkMode ? 'text-gray-300' : 'text-slate-300'
+      }`}
     >
       <div className="flex flex-col items-center gap-2 font-medium">
         {/* Header Image */}
-        <div className="relative w-10 h-10 sm:w-12 sm:h-12 mx-auto my-auto">
+        <div
+          className="relative mx-auto my-auto"
+          style={{ width: 'min(44px, 25vw)', height: 'min(44px, 25vw)' }}
+        >
           <Image
             src="/header.avif"
             alt="CadexLaw Logo"
-            width={144}
-            height={144}
-            className="w-12 h-12 sm:mr-4 mb-4"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-full"
             unoptimized={true}
+            quality={'100'}
           />
         </div>
         {/* Text Content */}
         <h4
-          className={
-            'text-xl px-3 sm:text-2xl goldGradient font-medium sm:px-4 ' +
-            poppins.className
-          }
+          className={`text-xl px-3 sm:text-2xl goldGradient font-medium sm:px-4 ${poppins.className}`}
         >
           CadexLaw
         </h4>
-        <p
-          className={
-            isDarkMode
-              ? 'text-gray-200'
-              : 'text-blue-950'
-          }
-        >
+        <p className={isDarkMode ? 'text-gray-200' : 'text-blue-950'}>
           Elevate your legal studies
           <br />
           Dive into interactive case simulations to enhance your skills
