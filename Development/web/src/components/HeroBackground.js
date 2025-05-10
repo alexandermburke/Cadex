eb2ac851327e1207eeae3c7a3a71e5bff6001828
Alex Burke <10841351+SupremacyXI@users.noreply.github.com>
2025-05-10 09:14:39 -0700

    Update HeroBackground.js

--- Development/web/src/components/HeroBackground.js
+++ Development/web/src/components/HeroBackground.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HeroBackground = () => (
-  <div className="absolute inset-0 w-full h-full flex items-start justify-center overflow-hidden opacity-55 drop-shadow-lg sm:scale-100 sm:mt-52 md:scale-125 md:mt-72 lg:scale-150 lg:mt-80">
+  <div className="absolute inset-0 w-full h-full flex items-start justify-center overflow-hidden opacity-55 drop-shadow-lg sm:scale-100 sm:mt-36 md:scale-125 md:mt-52 lg:scale-175 lg:mt-80">
     <svg
       className="absolute w-[1520px] h-full left-1/2 -translate-x-1/2 -translate-y-1/4"
       viewBox="0 0 1520 448"
